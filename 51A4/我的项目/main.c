8bd7d5902b87a4e43c11c3f05add79088c54592d
Fenobi <13667739122@qq.com>
2022-11-23 20:44:50 +0800

    修改

--- 51A4/我的项目/main.c
+++ 51A4/我的项目/main.c
@@ -3,12 +3,24 @@
 sbit led1=P2^0;
 sbit led2=P2^1;
 
+void delay500us(void)   //Îó²î 0us
+{
+    unsigned char a,b;
+    for(b=99;b>0;b--)
+        for(a=1;a>0;a--);
+}
+
 void main()
 {
 	while(1)
 	{
 		led1=0;
+		delay500us();
+		led1=1;
+		delay500us();
 		led2=0;
+		delay500us();
+		led2=1;
 
 	}
 }
